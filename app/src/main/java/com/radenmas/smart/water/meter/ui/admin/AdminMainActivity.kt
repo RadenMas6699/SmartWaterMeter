f7d66b0578189526101d6b0f21779b15f86829d1
RadenMas6699 <radenmas.dev@gmail.com>
2022-07-13 13:29:53 +0800

    Export v1.8

--- app/src/main/java/com/radenmas/smart/water/meter/ui/admin/AdminMainActivity.kt
+++ app/src/main/java/com/radenmas/smart/water/meter/ui/admin/AdminMainActivity.kt
@@ -5,10 +5,17 @@
 
 package com.radenmas.smart.water.meter.ui.admin
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
+import com.radenmas.smart.water.meter.R
 import com.radenmas.smart.water.meter.databinding.ActivityAdminMainBinding
 
 class AdminMainActivity : AppCompatActivity() {
@@ -22,5 +29,59 @@ class AdminMainActivity : AppCompatActivity() {
         b = ActivityAdminMainBinding.inflate(layoutInflater)
         setContentView(b.root)
 
+        val versionName = packageManager.getPackageInfo(packageName, 0).versionName
+        val packageName = packageManager.getPackageInfo(packageName, 0).packageName
+        val appName = resources.getString(R.string.app_name)
+
+        checkUpdate(appName, packageName, versionName)
+    }
+
+    private fun checkUpdate(appName: String, packageName: String, versionName: String) {
+        val defaultsRate = HashMap<String, Any>()
+        defaultsRate["lates_app_version"] = versionName
+        val configSettings = FirebaseRemoteConfigSettings.Builder()
+            .setMinimumFetchIntervalInSeconds(3600)
+            .build()
+        val config: FirebaseRemoteConfig = FirebaseRemoteConfig.getInstance()
+        config.setConfigSettingsAsync(configSettings)
+        config.setDefaultsAsync(defaultsRate)
+        config.fetchAndActivate().addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                val newVersionCode = config.getString("lates_app_version")
+                val lateAppVersion = newVersionCode.toFloat()
+                if (lateAppVersion > versionName.toFloat()) {
+                    showTheDialog(appName, packageName, newVersionCode)
+                }
+            }
+        }
+    }
+
+    private fun showTheDialog(appName: String, packageName: String, versionName: String) {
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("Update Aplikasi")
+            .setMessage("Versi terbaru sudah tersedia! Update aplikasi $appName ke versi: $versionName")
+            .setPositiveButton("Update", null)
+            .show()
+
+        dialog.setCancelable(false)
+
+        val positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+        positiveButton.setOnClickListener {
+            val uriUpdate =
+                "https://play.google.com/store/apps/details?id=$packageName"
+            val uri = Uri.parse(uriUpdate)
+            val updatePlayStore = Intent(Intent.ACTION_VIEW, uri)
+            updatePlayStore.setPackage("com.android.vending")
+            try {
+                startActivity(updatePlayStore)
+            } catch (e: ActivityNotFoundException) {
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse(uriUpdate)
+                    )
+                )
+            }
+        }
     }
 }
--- app/src/main/java/com/radenmas/smart/water/meter/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/radenmas/smart/water/meter/ui/splash/SplashActivity.kt
@@ -5,19 +5,10 @@
 
 package com.radenmas.smart.water.meter.ui.splash
 
-import android.content.ActivityNotFoundException
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.remoteconfig.FirebaseRemoteConfig
-import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 import com.radenmas.smart.water.meter.R
 import com.radenmas.smart.water.meter.databinding.ActivitySplashBinding
-import com.radenmas.smart.water.meter.ui.auth.AuthActivity
 
 class SplashActivity : AppCompatActivity() {
     private lateinit var b: ActivitySplashBinding
@@ -28,64 +19,7 @@ class SplashActivity : AppCompatActivity() {
         setContentView(b.root)
 
         val versionName = packageManager.getPackageInfo(packageName, 0).versionName
-        val packageName = packageManager.getPackageInfo(packageName, 0).packageName
-        val appName = resources.getString(R.string.app_name)
         b.tvAppVersion.text = resources.getString(R.string.app_version, versionName)
 
-        checkUpdate(appName, packageName, versionName)
-    }
-
-    private fun checkUpdate(appName: String, packageName: String, versionName: String) {
-        val defaultsRate = HashMap<String, Any>()
-        defaultsRate["lates_app_version"] = versionName
-        val configSettings = FirebaseRemoteConfigSettings.Builder()
-            .setMinimumFetchIntervalInSeconds(3600)
-            .build()
-        val config: FirebaseRemoteConfig = FirebaseRemoteConfig.getInstance()
-        config.setConfigSettingsAsync(configSettings)
-        config.setDefaultsAsync(defaultsRate)
-        config.fetchAndActivate().addOnCompleteListener(this) { task ->
-            if (task.isSuccessful) {
-                val newVersionCode = config.getString("lates_app_version")
-                val lateAppVersion = newVersionCode.toFloat()
-                if (lateAppVersion > versionName.toFloat()) {
-                    showTheDialog(appName, packageName, newVersionCode)
-                } else {
-                    Handler(Looper.getMainLooper()).postDelayed({
-                        startActivity(Intent(this, AuthActivity::class.java))
-                        finish()
-                    }, 1500)
-                }
-            }
-        }
-    }
-
-    private fun showTheDialog(appName: String, packageName: String, versionName: String) {
-        val dialog = AlertDialog.Builder(this)
-            .setTitle("Update Aplikasi")
-            .setMessage("Versi terbaru sudah tersedia! Update aplikasi $appName ke versi: $versionName")
-            .setPositiveButton("Update", null)
-            .show()
-
-        dialog.setCancelable(false)
-
-        val positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE)
-        positiveButton.setOnClickListener {
-            val uriUpdate =
-                "https://play.google.com/store/apps/details?id=$packageName"
-            val uri = Uri.parse(uriUpdate)
-            val updatePlayStore = Intent(Intent.ACTION_VIEW, uri)
-            updatePlayStore.setPackage("com.android.vending")
-            try {
-                startActivity(updatePlayStore)
-            } catch (e: ActivityNotFoundException) {
-                startActivity(
-                    Intent(
-                        Intent.ACTION_VIEW,
-                        Uri.parse(uriUpdate)
-                    )
-                )
-            }
-        }
     }
 }
